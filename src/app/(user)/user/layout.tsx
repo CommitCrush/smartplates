93023535aee6bc6e7cf420de806c18e20b5c58a2
Monika-Choudhary <monikachahota@gmail.com>
2025-10-14 21:25:04 +0200

    feat: Update UserLayout and WelcomePage for improved responsiveness and user experience

--- src/app/(user)/user/layout.tsx
+++ src/app/(user)/user/layout.tsx
@@ -6,7 +6,7 @@ interface UserLayoutProps {
 export default function UserLayout({ children }: UserLayoutProps) {
   return (
     <Layout>
-      <div className="max-w-[2000px] mx-auto py-6 px-2 sm:px-4 lg:px-6 xl:px-8">
+      <div >
         {children}
       </div>
     </Layout>
